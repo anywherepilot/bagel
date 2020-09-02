07e855f611660fc954be7dedc54d6553acf7656b
Jeroen Rietveld <anywherepilot@github.com>
2020-09-02 12:37:52 +0900

    Try post

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function bakeGreatBagels(slackApiToken) {
 
   // Get the list of channels
   let allChannels;
-  request.get("https://slack.com/api/conversations.list", { form: { token: slackApiToken } }, function (error, response, body) {
-      if(response.statusCode != 200 || !body.ok) core.setFailed(body.error);
-      allChannels = response.channels;
-      console.log(JSON.stringify(response));
+  request.post("https://slack.com/api/conversations.list", { form: { token: slackApiToken } }, function (error, response, body) {
+    if (response.statusCode != 200 || !body.ok) core.setFailed(body.error);
+    allChannels = response.channels;
+    console.log(JSON.stringify(response));
   });
 
   for (let channel of pairingChannels) {
